7a782ff2ba2e9d53a3955b7af8da3ad34570c4db
Simon Forsberg <zomis2k@hotmail.com>
2017-10-14 16:12:52 +0200

    Add a ChessGame component that is used from HelloWorld

---
+++ src/components/ChessGame.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="game chess">
+    <h1>{{ msg }}</h1>
+
+    <div class="board">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ChessGame",
+  data() {
+    return {
+      msg: "This is a chess board"
+    };
+  }
+};
+</script>
+
+<style scoped>
+</style>
