b814826d55e0bd3fe87287f1b44ca61dfa86fc6a
Simon Forsberg <zomis2k@hotmail.com>
2017-10-21 20:23:16 +0200

    Add startGame functionality

--- src/components/FlexGame.vue
+++ src/components/FlexGame.vue
@@ -6,6 +6,7 @@
     </div>
     <div class="awaiting-start" v-if="!summary.started">
       Game is not started yet.
+      <button @click="startGame()">Start game</button>
     </div>
     <div class="game flex" v-if="summary.started">
       <component :is="view" :games="games" :gameId="id"
@@ -24,26 +25,43 @@ export default {
       details: {}
     };
   },
+  methods: {
+    startGame: function() {
+      console.log("game id is " + this.id);
+      this.games.start2({ gameId: this.id }).then(
+        response => {
+          console.log(response.body);
+          this.fetchSummary();
+        },
+        err => {
+          console.log(err);
+        }
+      );
+    },
+    fetchSummary: function() {
+      this.games.summary({ gameId: this.id }).then(
+        response => {
+          this.summary = response.body;
+        },
+        err => {
+          console.log(err);
+        }
+      );
+    }
+  },
   created: function() {
     var customActions = {
       summary: { method: "GET", url: this.game + "{/gameId}" },
       join: { method: "POST", url: this.game + "{/gameId}/join" },
       details: { method: "GET", url: this.game + "{/gameId}/details" },
-      start: { method: "POST", url: this.game + "{/gameId}/start" },
+      start2: { method: "POST", url: this.game + "/" + this.id + "/start" },
       action: {
         method: "POST",
         url: this.game + "{/gameId}/actions{/type}?token={token}"
       }
     };
     this.games = this.$resource(this.game, {}, customActions);
-    this.games.summary({ gameId: this.id }).then(
-      response => {
-        this.summary = response.body;
-      },
-      err => {
-        console.log(err);
-      }
-    );
+    this.fetchSummary();
   },
   computed: {}
 };
