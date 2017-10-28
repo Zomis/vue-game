d3bed5deb811d6266ee5e4f03cb712a943ab29ea
Simon Forsberg <zomis2k@hotmail.com>
2017-10-28 21:06:15 +0200

    Add the royal game of UR

--- src/router/index.js
+++ src/router/index.js
@@ -8,6 +8,7 @@ import CreateFlexGame from "@/components/CreateFlexGame";
 import PreFlexGame from "@/components/PreFlexGame";
 import GridView from "@/components/views/GridView";
 import TTTView from "@/components/ttt/TTTView";
+import RoyalGameOfURView from "@/components/ttt/RoyalGameOfURView";
 import VueResource from "vue-resource";
 
 Vue.use(Router);
@@ -43,6 +44,26 @@ export default new Router({
       props: route => ({
         name: route.query.name,
         game: "ttt"
+      })
+    },
+    {
+      path: "/ur",
+      name: "RoyalGameOfUR",
+      component: LobbyScreen,
+      props: route => ({
+        name: route.query.name,
+        game: "ur"
+      })
+    },
+    {
+      path: "/ur/:id",
+      name: "RoyalGameOfURGame",
+      component: FlexGame,
+      props: route => ({
+        id: route.params.id,
+        token: route.query.token,
+        game: "ur",
+        view: RoyalGameOfURView
       })
     },
     {
